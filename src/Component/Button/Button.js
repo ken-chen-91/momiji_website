e2a669bd3cd78a4284c56f90be8c919c87125776
kenchen <a330120231@gmail.com>
2021-08-18 03:21:09 -0700

    bug

--- src/Component/Button/Button.js
+++ src/Component/Button/Button.js
@@ -4,9 +4,16 @@ import { Link } from "react-router-dom";
 
 const Button = (props) => {
     return (
-        <Link to="sign-up">
-            <button className="btn">{props.label}</button>
-        </Link>
+        <>
+            {/* <Link to="sign-up">
+               <button className="btn">{props.label}</button>
+            </Link> */}
+            
+                <a href="https://momiji-west-linn.hrpos.heartland.us/menu"><button className="btn">{props.label}</button></a>
+          
+            </>
+        
+        
     );
 };
 
